c19c129081afd2c510369dcd88347737fbcf271f
shweta <junnarkarshweta3@gmail.com>
2023-09-18 17:53:12 +0530

    Inheritance Types.

---
+++ TypesOfInheritance.java
@@ -0,0 +1,19 @@
+public class TypesOfInheritance {
+
+}
+class SingleLevel extends TypesOfInheritance// One parent One child (single level inheritance.
+{
+
+}
+class MultiLevel extends SingleLevel//A->B->C  it is a multi level inheritance
+{
+
+}
+class Hirarchy extends TypesOfInheritance//One parent have two childs, it is a Hirarchy inheritance.
+{
+
+}
+//class TwoParent extends TypesOfInheritance, SingleLevel // two parent not possible . it is a multiple inheritance.
+//{
+//
+//}
